11423ed43c5261bf22e8aec931d937afc56da1da
Mark Aquino <markaquino@gmail.com>
2025-09-02 14:17:31 +0800

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,27 @@
 async function generatePlaylist() {
   const mood = document.getElementById("moodInput").value;
-  const res = await fetch(`http://localhost:5000/playlist?mood=${encodeURIComponent(mood)}`);
+
+  const res = await fetch(`https://moodify-backend-9a9p.onrender.com/playlist?mood=${encodeURIComponent(mood)}`);
   const data = await res.json();
+
   const resultsDiv = document.getElementById("results");
   resultsDiv.innerHTML = "";
-  data.playlist.forEach((song) => {
-    const div = document.createElement("div");
-    div.innerHTML = `<h3>${song.title} - ${song.artist}</h3>
-      <iframe width="300" height="200" src="https://www.youtube.com/embed/${song.youtubeId}" frameborder="0" allowfullscreen></iframe>`;
-    resultsDiv.appendChild(div);
-  });
+
+  if (data.playlist && data.playlist.length > 0) {
+    data.playlist.forEach((song) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <h3>${song.title} - ${song.artist}</h3>
+        <iframe 
+          width="300" 
+          height="200" 
+          src="https://www.youtube.com/embed/${song.youtubeId}" 
+          frameborder="0" 
+          allowfullscreen>
+        </iframe>`;
+      resultsDiv.appendChild(div);
+    });
+  } else {
+    resultsDiv.innerHTML = "<p>No songs found. Try another mood 🎶</p>";
+  }
 }
